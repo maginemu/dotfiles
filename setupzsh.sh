4ad7ea8a0f5386ad09d4f0560fdbcc294325ad10
maginemu <lonely.two.mn+github@gmail.com>
2015-05-20 11:26:29 +0900

    Using zprezto instead of oh-my-zsh

--- setupzsh.sh
+++ setupzsh.sh
@@ -4,14 +4,18 @@
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 # install oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh || 0
+#curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh || 0
 
 # remove once
-mv ~/.zshrc ~/.zshrc.oh-my-zsh.bak
+#mv ~/.zshrc ~/.zshrc.oh-my-zsh.bak
 
 # symlink zshrc
-ln -sf $script_dir/zshrc.d ~/.zshrc.d
+#ln -sf $script_dir/zshrc.d ~/.zshrc.d
+#ln -sf $script_dir/.zshrc ~/.zshrc
+
+ln -sf $script_dir/zprezto ~/.zprezto
 ln -sf $script_dir/.zshrc ~/.zshrc
+
 
 # symlink bash_profile, bashrc
 ln -sf $script_dir/.bash_profile ~/.bash_profile
